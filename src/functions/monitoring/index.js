c05805ce2dba39493e382974867836ee6a75a173
Tomas <tomasholderness@gmail.com>
2018-05-16 10:13:46 -0400

    update tls param

--- src/functions/monitoring/index.js
+++ src/functions/monitoring/index.js
@@ -1,3 +1,4 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // Axios AWS cert issue
 import axios from 'axios';
 
 // Local objects
@@ -10,32 +11,29 @@ import config from '../../config';
  * @param {Object} context - AWS Lambda context object
  * @param {Object} callback - Callback (HTTP response)
  */
-export default async (event, context, callback) => {
-  try {
-    const response = await axios({
-        method:'get',
-        url:config.ENDPOINT,
-        responseType:'json',
-        rejectUnauthorized: false, // Axios issue with AWS certs
-        params: {
-            bbox: '-81,27,-79,25',
-            geoformat: 'geojson',
-        }
-      });
+export default (event, context, callback) => {
+    const p1 = axios.get(config.ENDPOINT, {
+    params: {
+        bbox: '-81,27,-79,25',
+        geoformat: 'geojson',
+        },
+    });
 
-    if (response.data.statusCode === 200) {
-        console.log('Recieve 200 response from ' + config.ENDPOINT);
-        callback(null, JSON.stringify(
-            'Received 200 response from ' + config.ENDPOINT));
-    } else {
-        console.log('Received non 200 response from ' + config.ENDPOINT);
-        callback(new Error(
-            'Received non 200 response from ' + config.ENDPOINT));
-    }
-  } catch (err) {
+    Promise.all([p1]).then((values) => {
+        const response = values[0];
+        if (response.data.statusCode === 200) {
+            console.log('Recieve 200 response from ' + config.ENDPOINT);
+            callback(null, JSON.stringify(
+                'Received 200 response from ' + config.ENDPOINT));
+        } else {
+            console.log('Received non 200 response from ' + config.ENDPOINT);
+            callback(new Error(
+                'Received non 200 response from ' + config.ENDPOINT));
+        }
+    }).catch((err) => {
         console.log(
             'Error requesting ' + config.ENDPOINT + ' - ' + err.message);
         callback(new Error(
             'Error requesting ' + config.ENDPOINT + ' - ' + err.message));
-  }
+    });
 };
