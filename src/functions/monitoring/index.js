a125b977ff07c2d71656841c3c58336e2de65aec
Tomas <tomasholderness@gmail.com>
2018-05-16 10:32:23 -0400

    fix lint

--- src/functions/monitoring/index.js
+++ src/functions/monitoring/index.js
@@ -4,21 +4,26 @@ import axios from 'axios';
 // Local objects
 import config from '../../config';
 
+/**
+ * Verify get sensors endpoint working
+ * @function getSensors
+ * @return {Promise} - Result of request and validation
+ */
 function getSensors() {
     return new Promise((resolve, reject) => {
-        axios.get(config.ENDPOINT, { 
-            params:{
+        axios.get(config.ENDPOINT, {
+            params: {
                 bbox: '-81,27,-79,25',
                 geoformat: 'geojson',
             },
         }).then((response) => {
             if (response.data.statusCode === 200) {
-                console.log('Recieve 200 response from ' + 
+                console.log('Recieve 200 response from ' +
                     config.ENDPOINT);
-                resolve('Received 200 response from ' + 
+                resolve('Received 200 response from ' +
                     config.ENDPOINT);
             } else {
-                console.log('Received non 200 response from ' + 
+                console.log('Received non 200 response from ' +
                     config.ENDPOINT);
                 reject(new Error(
                     'Received non 200 response from ' + config.ENDPOINT));
