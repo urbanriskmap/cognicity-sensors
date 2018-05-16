f7f114b1c5d695aa56acccc3029f244f6a3e01f9
Tomas <tomasholderness@gmail.com>
2018-05-16 11:05:26 -0400

    add support for get sensor data

--- src/functions/monitoring/index.js
+++ src/functions/monitoring/index.js
@@ -10,8 +10,9 @@ import config from '../../config';
  * @return {Promise} - Result of request and validation
  */
 function getSensors() {
+    const endpoint = config.ENDPOINT;
     return new Promise((resolve, reject) => {
-        axios.get(config.ENDPOINT, {
+        axios.get(endpoint, {
             params: {
                 bbox: '-81,27,-79,25',
                 geoformat: 'geojson',
@@ -19,14 +20,43 @@ function getSensors() {
         }).then((response) => {
             if (response.data.statusCode === 200) {
                 console.log('Recieve 200 response from ' +
-                    config.ENDPOINT);
+                    endpoint);
                 resolve('Received 200 response from ' +
-                    config.ENDPOINT);
+                    endpoint);
             } else {
                 console.log('Received non 200 response from ' +
-                    config.ENDPOINT);
+                    endpoint);
                 reject(new Error(
-                    'Received non 200 response from ' + config.ENDPOINT + '. Error was: ' + response.errorMessage));
+                    'Received non 200 response from ' + endpoint +
+                    '. Error was: ' + response.errorMessage));
+            }
+        }).catch((err) => reject(err));
+    });
+}
+
+/**
+ * Verify get sensors endpoint working
+ * @function getSensors
+ * @return {Promise} - Result of request and validation
+ */
+function getSensorData() {
+    const endpoint = config.ENDPOINT + '1';
+    return new Promise((resolve, reject) => {
+        axios.get(endpoint, {
+        }).then((response) => {
+            // Happy with a 404 if sensor not in database
+            if (response.data.statusCode === 200 ||
+                response.data.statusCode === 404) {
+                console.log('Recieve 200 or 404 response from ' +
+                    endpoint);
+                resolve('Received 200 or 404 response from ' +
+                    endpoint);
+            } else {
+                console.log('Received non 200 or 404 response from ' +
+                    endpoint);
+                reject(new Error(
+                    'Received non 200 or 404 response from ' + endpoint +
+                    '. Error was: ' + response.errorMessage));
             }
         }).catch((err) => reject(err));
     });
@@ -40,7 +70,8 @@ function getSensors() {
  * @param {Object} callback - Callback (HTTP response)
  */
 export default (event, context, callback) => {
-    getSensors()
+    // Make requests
+    Promise.all(getSensors(), getSensorData())
         .then((res) => callback(null, res))
         .catch((err) => callback(err));
 };
