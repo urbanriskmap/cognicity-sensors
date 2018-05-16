5fe67b74ca3a818ac060597499573c4c71964374
Tomas <tomasholderness@gmail.com>
2018-05-16 09:50:00 -0400

    add pass for axios on aws certs

--- src/functions/monitoring/index.js
+++ src/functions/monitoring/index.js
@@ -12,13 +12,17 @@ import config from '../../config';
  */
 export default async (event, context, callback) => {
   try {
-    const response = await axios.get(config.ENDPOINT, {
-    rejectUnauthorized: false,
-    params: {
-        bbox: '-81,27,-79,25',
-        geoformat: 'geojson',
-        },
-    });
+    const response = await axios({
+        method:'get',
+        url:config.ENDPOINT,
+        responseType:'json',
+        rejectUnauthorized: true, // Axios issue with AWS certs
+        params: {
+            bbox: '-81,27,-79,25',
+            geoformat: 'geojson',
+        }
+      });
+
     if (response.data.statusCode === 200) {
         console.log('Recieve 200 response from ' + config.ENDPOINT);
         callback(null, JSON.stringify(
